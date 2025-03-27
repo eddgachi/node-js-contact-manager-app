f25071ee28ff4c3913c9dcb1a526e2884f57028f
edwin gachira <edwingachira@gmail.com>
2025-03-27 05:29:48 +0300

    moved routing logic to controller

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', (req, res) => {
   res.status(200).json({ message: 'Hello World!' })
 })
 
-app.use('/api/contacts', require('./routes/contacts'))
+app.use('/api/contacts', require('./routes/contactRoutes'))
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`)
