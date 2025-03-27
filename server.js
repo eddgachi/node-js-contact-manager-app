c9210fe58e5580bde11447e55efab77ddc283d57
edwin gachira <edwingachira@gmail.com>
2025-03-27 05:16:34 +0300

    api set up && got application running

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const express = require('express')
+const dotenv = require('dotenv').config()
 
 const app = express()
 
-const port = 5000
+const port = process.env.PORT || 5000
+
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'Hello World!' })
+})
+
+app.use('/api/contacts', require('./routes/contacts'))
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`)
