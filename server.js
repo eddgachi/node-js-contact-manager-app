1e6fa883a724b225d39c2097cbd139d7e89ff153
edwin gachira <edwingachira@gmail.com>
2025-03-27 05:53:57 +0300

    added constants and error handling middleware

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const errorHandler = require('./middleware/errorHandler')
 const dotenv = require('dotenv').config()
 
 const app = express()
@@ -9,7 +10,9 @@ app.get('/', (req, res) => {
   res.status(200).json({ message: 'Hello World!' })
 })
 
+app.use(express.json())
 app.use('/api/contacts', require('./routes/contactRoutes'))
+app.use(errorHandler)
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`)
